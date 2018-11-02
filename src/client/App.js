20810a380946f8003019f04cf4e1230a261b3179
Robert Rusch <rusch@mit.edu>
2018-11-02 16:06:31 -0400

    Start messing around

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
 
+import AppBar from './AppBar';
+import SearchBar from './SearchBar';
+
+
+// eslint-disable-next-line react/prefer-stateless-function
 export default class App extends Component {
-  state = { username: null };
-
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
-
   render() {
-    const { username } = this.state;
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+        <AppBar />
+        <SearchBar />
       </div>
     );
   }
